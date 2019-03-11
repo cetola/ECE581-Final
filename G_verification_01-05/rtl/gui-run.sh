660213ab88fef678f617027ee9ea2673d02e52a3
Stephano Cetola <stephanoc@gmail.com>
2019-03-10 19:09:54 -0700

    cleanup gui-run for RTL sim
    No need to recompile. That's what run.sh is for.
    
    Also, don't forget -novopt option.
    
    Signed-off-by: Stephano Cetola <stephanoc@gmail.com>

--- G_verification_01-05/rtl/gui-run.sh
+++ G_verification_01-05/rtl/gui-run.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-echo Simulating toptb with -do run all
-echo Running vlog on *.sv
-vlog *.sv
-echo Run complete. Check for errors.
-echo Running vsim
-echo
-vsim toptb -do "wave.do"
+vsim -novopt toptb -do "wave.do"
